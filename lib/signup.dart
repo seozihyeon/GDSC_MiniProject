f812c318c9aaa2d19dfd194dab87c70116832cb8
서지현 <jihyun3317@naver.com>
2024-07-25 21:26:40 +0900

    0725 test

--- lib/signup.dart
+++ lib/signup.dart
@@ -34,13 +34,10 @@ class _SignupPageState extends State<SignupPage> {
           child: Icon(Icons.close),
         ),
       ),
-      body: Stack(
-        children: [
-          Positioned(
-            top: 0,
-            right: 0,
-            left: 0,
-            child: Container(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
               height: 270,
               decoration: BoxDecoration(
                 color: Color(0xFF76A9E6),
@@ -70,12 +67,7 @@ class _SignupPageState extends State<SignupPage> {
                 ),
               ),
             ),
-          ),
-          Positioned(
-            top: 120,
-            left: 30,
-            right: 30,
-            child: Container(
+            Container(
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(8.0),
@@ -92,145 +84,143 @@ class _SignupPageState extends State<SignupPage> {
                   ),
                 ],
               ),
-              padding: EdgeInsets.only(top: 10, bottom: 0, left: 24, right: 24),
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      child: Text(
-                        "회원 정보를 입력해주세요.",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      margin: EdgeInsets.all(20),
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 24),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Text(
+                      "회원 정보를 입력해주세요.",
+                      style: TextStyle(fontWeight: FontWeight.bold),
                     ),
-                    TextField(
-                      controller: _UsernameController,
-                      decoration: InputDecoration(
-                        labelText: '성명',
-                        labelStyle: TextStyle(
-                          color: Colors.grey.shade800, // 기본 라벨 색상
-                        ),
-                        floatingLabelStyle: TextStyle(
-                          color: Colors.grey.shade800, // 포커스 상태의 라벨 색상
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Color(0xFF76A9E6),
-                          ),
+                    margin: EdgeInsets.all(20),
+                  ),
+                  TextField(
+                    controller: _UsernameController,
+                    decoration: InputDecoration(
+                      labelText: '성명',
+                      labelStyle: TextStyle(
+                        color: Colors.grey.shade800, // 기본 라벨 색상
+                      ),
+                      floatingLabelStyle: TextStyle(
+                        color: Colors.grey.shade800, // 포커스 상태의 라벨 색상
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0xFF76A9E6),
                         ),
                       ),
                     ),
-                    SizedBox(height: 12.0),
-                    TextField(
-                      controller: _UseridController,
-                      decoration: InputDecoration(
-                        labelText: '아이디',
-                        labelStyle: TextStyle(
-                          color: Colors.grey.shade800, // 기본 라벨 색상
-                        ),
-                        floatingLabelStyle: TextStyle(
-                          color: Colors.grey.shade800, // 포커스 상태의 라벨 색상
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Color(0xFF76A9E6)),
+                  ),
+                  SizedBox(height: 12.0),
+                  TextField(
+                    controller: _UseridController,
+                    decoration: InputDecoration(
+                      labelText: '아이디',
+                      labelStyle: TextStyle(
+                        color: Colors.grey.shade800, // 기본 라벨 색상
+                      ),
+                      floatingLabelStyle: TextStyle(
+                        color: Colors.grey.shade800, // 포커스 상태의 라벨 색상
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF76A9E6)),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 12.0),
+                  TextField(
+                    controller: _PasswordController,
+                    decoration: InputDecoration(
+                      labelText: '비밀번호',
+                      labelStyle: TextStyle(
+                        color: Colors.grey.shade800, // 기본 라벨 색상
+                      ),
+                      floatingLabelStyle: TextStyle(
+                        color: Colors.grey.shade800, // 포커스 상태의 라벨 색상
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0xFF76A9E6),
                         ),
                       ),
                     ),
-                    SizedBox(height: 12.0),
-                    TextField(
-                      controller: _PasswordController,
-                      decoration: InputDecoration(
-                        labelText: '비밀번호',
-                        labelStyle: TextStyle(
-                          color: Colors.grey.shade800, // 기본 라벨 색상
-                        ),
-                        floatingLabelStyle: TextStyle(
-                          color: Colors.grey.shade800, // 포커스 상태의 라벨 색상
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Color(0xFF76A9E6),
-                          ),
+                  ),
+                  SizedBox(height: 12.0),
+                  TextField(
+                    controller: _NeighborhoodController,
+                    decoration: InputDecoration(
+                      labelText: '우리 동네',
+                      labelStyle: TextStyle(
+                        color: Colors.grey.shade800, // 기본 라벨 색상
+                      ),
+                      floatingLabelStyle: TextStyle(
+                        color: Colors.grey.shade800, // 포커스 상태의 라벨 색상
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0xFF76A9E6),
                         ),
                       ),
                     ),
-                    SizedBox(height: 12.0),
-                    TextField(
-                      controller: _NeighborhoodController,
-                      decoration: InputDecoration(
-                        labelText: '우리 동네',
-                        labelStyle: TextStyle(
-                          color: Colors.grey.shade800, // 기본 라벨 색상
-                        ),
-                        floatingLabelStyle: TextStyle(
-                          color: Colors.grey.shade800, // 포커스 상태의 라벨 색상
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Color(0xFF76A9E6),
-                          ),
+                  ),
+                  SizedBox(height: 12.0),
+                  TextField(
+                    controller: _AddressController,
+                    decoration: InputDecoration(
+                      labelText: '주소',
+                      labelStyle: TextStyle(
+                        color: Colors.grey.shade800, // 기본 라벨 색상
+                      ),
+                      floatingLabelStyle: TextStyle(
+                        color: Colors.grey.shade800, // 포커스 상태의 라벨 색상
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0xFF76A9E6),
                         ),
                       ),
                     ),
-                    SizedBox(height: 12.0),
-                    TextField(
-                      controller: _AddressController,
-                      decoration: InputDecoration(
-                        labelText: '주소',
-                        labelStyle: TextStyle(
-                          color: Colors.grey.shade800, // 기본 라벨 색상
-                        ),
-                        floatingLabelStyle: TextStyle(
-                          color: Colors.grey.shade800, // 포커스 상태의 라벨 색상
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Color(0xFF76A9E6),
-                          ),
+                  ),
+                  SizedBox(height: 12.0),
+                  TextField(
+                    controller: _PhoneNumberController,
+                    decoration: InputDecoration(
+                      labelText: '휴대폰 번호',
+                      labelStyle: TextStyle(
+                        color: Colors.grey.shade800, // 기본 라벨 색상
+                      ),
+                      floatingLabelStyle: TextStyle(
+                        color: Colors.grey.shade800, // 포커스 상태의 라벨 색상
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0xFF76A9E6),
                         ),
                       ),
                     ),
-                    SizedBox(height: 12.0),
-                    TextField(
-                      controller: _PhoneNumberController,
-                      decoration: InputDecoration(
-                        labelText: '휴대폰 번호',
-                        labelStyle: TextStyle(
-                          color: Colors.grey.shade800, // 기본 라벨 색상
-                        ),
-                        floatingLabelStyle: TextStyle(
-                          color: Colors.grey.shade800, // 포커스 상태의 라벨 색상
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Color(0xFF76A9E6),
-                          ),
-                        ),
-                      ),
+                  ),
+                  SizedBox(height: 20.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(builder: (context) => MainPage()),
+                            (route) => false,
+                      );
+                    },
+                    style: ButtonStyle(
+                      backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black54),
                     ),
-                    SizedBox(height: 20.0),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushAndRemoveUntil(
-                          context,
-                          MaterialPageRoute(builder: (context) => MainPage()),
-                              (route) => false,
-                        );
-                      },
-                      style: ButtonStyle(
-                        backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.black54),
-                      ),
-                      child: Text('회원가입', style: TextStyle(color: Colors.white)),
-                    ),
-                    SizedBox(height: 20.0),
-                  ],
-                ),
+                    child: Text('회원가입', style: TextStyle(color: Colors.white)),
+                  ),
+                  SizedBox(height: 20.0),
+                ],
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
